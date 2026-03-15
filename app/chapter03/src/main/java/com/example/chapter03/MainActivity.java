e2329273a0f4477be819d3aa841ed65b11a867a7
Aphrodite <1586113213@qq.com>
2026-03-15 23:42:07 +0800

    finish calculator pages

--- app/chapter03/src/main/java/com/example/chapter03/MainActivity.java
+++ app/chapter03/src/main/java/com/example/chapter03/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.chapter03;
 
 import android.os.Bundle;
-import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +16,16 @@ public class MainActivity extends AppCompatActivity {
         Button btn = findViewById(R.id.btn_single);
         TextView tv_result = findViewById(R.id.tv_result);
         btn.setOnClickListener(new MyOnClickListener(tv_result));
+
+        Button btn_long_click = findViewById(R.id.btn_long);
+        // 使用Lambda表达式设置长按监听器
+        btn_long_click.setOnLongClickListener(v -> {
+            // 其中的v表示调用该方法的对象，这里指btn_long_click
+            String desc = String.format("按钮 %s 被长按了", ((Button) v).getText());
+            tv_result.setText(desc);
+            return true;
+        });
+
     }
 
     static class MyOnClickListener implements View.OnClickListener {
@@ -32,4 +39,5 @@ public class MainActivity extends AppCompatActivity {
             tv_result.setText(desc);
         }
     }
+
 }
